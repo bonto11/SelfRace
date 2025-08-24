647ccbbb3e3b5926d90740e78a3ac77277c89ca4
bonto11 <patrikmbontar@gmail.com>
2025-08-24 14:36:31 +0200

    funguje, zapisuje a cita do/z vsetkych tabuliek

--- Backend/Modules/User/user_handler.py
+++ Backend/Modules/User/user_handler.py
@@ -1,10 +1,16 @@
 from typing import Optional
 from Modules.SQL.db_handler import get_service_client
 
-supabase = get_service_client()
 TABLE_USERS = "users"
+TABLE_USERS_PROFILE = "users_profile"
+TABLE_USERS_ZONES = "users_zones"
+TABLE_USERS_THRESHOLDS = "users_thresholds"
+TABLE_USERS_BESTS = "users_bests"
+TABLE_USERS_RECOVERY = "users_recovery"
 
-def create_user(name: str, age: int, mail_address: str, primary_sport: str,
+supabase = get_service_client()
+
+def create_user(name: str, age: int, mail_address: str,
                 display_name: Optional[str] = None, auth_uid: Optional[str] = None):
     exists = supabase.table(TABLE_USERS).select("id").eq("mail_address", mail_address).limit(1).execute()
     if exists.data:
@@ -15,7 +21,6 @@ def create_user(name: str, age: int, mail_address: str, primary_sport: str,
         "name": name,
         "age": age,
         "mail_address": mail_address,
-        "primary_sport": primary_sport,
         "display_name": display_name or name,
     }
     if auth_uid:
@@ -38,7 +43,6 @@ def delete_user(mail_address: str):
 
 def get_or_create_user_id(email: str, *,
                           name: str = "New User",
-                          primary_sport: str = "running",
                           display_name: Optional[str] = None,
                           auth_uid: Optional[str] = None) -> int:
     user = get_user_by_email(email)
@@ -48,7 +52,6 @@ def get_or_create_user_id(email: str, *,
             name=name,
             age=0,
             mail_address=email,
-            primary_sport=primary_sport,
             display_name=display_name or name,
             auth_uid=auth_uid
         )
@@ -56,3 +59,61 @@ def get_or_create_user_id(email: str, *,
         if not user:
             raise RuntimeError("Nepodarilo sa vytvoriť používateľa")
     return user[0]["id"]
+
+# --- USER PROFILE ---
+def insert_or_update_user_profile(user_id: int, **fields):
+    payload = {"user_id": user_id, **fields}
+    resp = supabase.table(TABLE_USERS_PROFILE).upsert(payload).execute()
+    print("✅ user_profile uložené:", resp.data)
+    return resp.data
+
+def get_user_profile(user_id: int):
+    return supabase.table(TABLE_USERS_PROFILE).select("*").eq("user_id", user_id).limit(1).execute().data
+
+
+# --- USER ZONES ---
+def insert_or_update_user_zones(user_id: int, **fields):
+
+    payload = {"user_id": user_id, **fields}
+    resp = supabase.table(TABLE_USERS_ZONES).upsert(payload).execute()
+    print("✅ user_zones uložené:", resp.data)
+    return resp.data
+
+def get_user_zones(user_id: int):
+    return supabase.table(TABLE_USERS_ZONES).select("*").eq("user_id", user_id).limit(1).execute().data
+
+
+# --- USER THRESHOLDS ---
+def insert_or_update_user_thresholds(user_id: int, **fields):
+
+    payload = {"user_id": user_id, **fields}
+    resp = supabase.table(TABLE_USERS_THRESHOLDS).upsert(payload).execute()
+    print("✅ user_thresholds uložené:", resp.data)
+    return resp.data
+
+def get_user_thresholds(user_id: int):
+    return supabase.table(TABLE_USERS_THRESHOLDS).select("*").eq("user_id", user_id).limit(1).execute().data
+
+
+# --- USER BESTS ---
+def insert_or_update_user_bests(user_id: int, **fields):
+
+    payload = {"user_id": user_id, **fields}
+    resp = supabase.table(TABLE_USERS_BESTS).upsert(payload).execute()
+    print("✅ user_bests uložené:", resp.data)
+    return resp.data
+
+def get_user_bests(user_id: int):
+    return supabase.table(TABLE_USERS_BESTS).select("*").eq("user_id", user_id).limit(1).execute().data
+
+# --- USER DAILY RECOVERY ---
+def insert_or_update_user_recovery(user_id: int, **fields):
+
+    payload = {"user_id": user_id, **fields}
+    resp = supabase.table(TABLE_USERS_RECOVERY).upsert(payload).execute()
+    print("✅ user_daily_recovery uložené:", resp.data)
+    return resp.data
+
+def get_user_recovery(user_id: int):
+    return supabase.table(TABLE_USERS_RECOVERY).select("*").eq("user_id", user_id).limit(1).execute().data
+
--- Backend/Modules/main.py
+++ Backend/Modules/main.py
@@ -337,10 +337,41 @@ def main():
     #sync_history(user_id, datetime(2023,5,1,tzinfo=timezone.utc), datetime.now(timezone.utc))
 
     # 2) SYNC – nastav si, či chceš „force posledných 30 dní“ a či chceš RAW archív
-    sync_activities(user_id, force_full_30d=False, archive_raw=False)
+    #sync_activities(user_id, force_full_30d=False, archive_raw=False)
+    
+     # Test – vlož profil
+        #user_hdl.insert_or_update_user_profile(user_id, weight_kg=82.3, height_cm=186, body_fat_pct=7.7, HR_max=201, RHR=52, birth_date = "1996-11-19", VO2Max = 46.5)
 
+    # Test – vlož zóny
+        #user_hdl.insert_or_update_user_zones(user_id, sport = "running", zone_type = 1, HR_min_bpm=120, HR_max_bpm=147)
+        #user_hdl.insert_or_update_user_zones(user_id, sport = "running", zone_type = 2, HR_min_bpm=148, HR_max_bpm=164)
+        #user_hdl.insert_or_update_user_zones(user_id, sport = "running", zone_type = 3, HR_min_bpm=165, HR_max_bpm=175)
+        #user_hdl.insert_or_update_user_zones(user_id, sport = "running", zone_type = 4, HR_min_bpm=176, HR_max_bpm=184)
+        #user_hdl.insert_or_update_user_zones(user_id, sport = "running", zone_type = 5, HR_min_bpm=185, HR_max_bpm=201)
+
+    # Test – vlož prahy
+        #user_hdl.insert_or_update_user_thresholds(user_id, sport = "running", threshold_type = "LT2", HR_bpm=184, pace_sec_km=295, measurement_type = "estimate garmin")
+    
+        #user_hdl.insert_or_update_user_thresholds(user_id, sport = "running", threshold_type = LT1, HR_bpm=164, measurement_type = "laboratory test")
+        #user_hdl.insert_or_update_user_thresholds(user_id, sport = "running", threshold_type = MLSS, value=10)
+        #user_hdl.insert_or_update_user_thresholds(user_id, sport = "cycling", threshold_type = LT2, HR_bpm=184, power_watt=295)
+
+    # Test – vlož osobáky
+        #user_hdl.insert_or_update_user_bests(user_id, distance_m = 400, best_time_s=1393)
+        #user_hdl.insert_or_update_user_bests(user_id, distance_m = 10000, best_time_s=3017)
+        #user_hdl.insert_or_update_user_bests(user_id, distance_m = 21097, best_time_s=8527)
+        
+    # Test – vlož resting_values
+    #   user_hdl.insert_or_update_user_recovery(user_id, date = "2025-08-24", RHR_bpm=53, HRV_avg_ms=64, HRV_max_ms=86, sleep_duration_min = 487, sleep_start_timestampz = "2025-08-23T23:10:00+02:00", alcohol_volume_ml = 1000, alcohol_type_pct = 12, food_2h_before = False)
+ 
+    #print(user_hdl.get_user_profile(user_id))
+    #print(user_hdl.get_user_zones(user_id))
+    #print(user_hdl.get_user_thresholds(user_id))
+    #print(user_hdl.get_user_bests(user_id))
+    #print(user_hdl.get_user_recovery(user_id))
+    
     # 3) Report (pracuje nad activities_summary)
-    reporting.generate_report(user_id)
+    #reporting.generate_report(user_id)
 
     # 4) STREAMS pre jednu aktivitu (príklad)
     #activity_id = 15342917851
