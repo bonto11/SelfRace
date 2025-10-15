660f3f9a7116c9d336a58534e3e618726843a575
bonto11 <157644354+bonto11@users.noreply.github.com>
2025-10-15 21:14:36 +0200

    Update ClientPage.tsx

--- frontend/src/app/(protected)/recovery/ClientPage.tsx
+++ frontend/src/app/(protected)/recovery/ClientPage.tsx
@@ -6,7 +6,7 @@ import WidgetRHR from "@/features/widgets/WidgetRHR";
 import WidgetHRV from "@/features/widgets/WidgetHRV";
 import WidgetSleepDuration from "@/features/widgets/WidgetSleepDuration";
 import WidgetSleepStart from "@/features/widgets/WidgetSleepStart";
-import InputsCard from "@/features/recovery/components/RecoveryInputsCard";
+import InputsCard from "@/features/recovery/components/InputsCard";
 import RecoveryDataProvider, { useRecoveryData } from "@/features/recovery/data/RecoveryDataContext";
 import { ArrowPathIcon } from "@heroicons/react/24/outline"; // refresh ikona
 
