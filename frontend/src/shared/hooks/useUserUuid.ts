379558c42778d57e0ac66b1a589f0586b2b10e20
z004es1x <PATRIKMBONTAR@GMAIL.COM>
2025-10-17 14:19:16 +0200

    old

--- frontend/src/shared/hooks/useUserUuid.ts
+++
@@ -1,8 +0,0 @@
-// src/shared/hooks/useUserUuid.ts
-"use client";
-import { useUserId } from "./useUserId";
-
-export function useUserUuid() {
-  const { uuid } = useUserId();
-  return { uuid };
-}
