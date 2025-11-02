7a4a7ffa1e41d16cfbee705da1d29b49f664da10
z004es1x <PATRIKMBONTAR@GMAIL.COM>
2025-11-02 12:54:23 +0100

    edit calendar

--- frontend/src/shared/components/calendar/CalendarMonth.tsx
+++ frontend/src/shared/components/calendar/CalendarMonth.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Button from "./ui/Button";
+import Button from "@/shared/components/ui/Button";
 
 /* -------- Farby športov (dočasne natvrdo) --------
    Neskôr nahradíš za THEME / user-prefs. */
