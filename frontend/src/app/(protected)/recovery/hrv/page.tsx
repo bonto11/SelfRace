b700b3bcf67792584fa924d70a67b0e25122f129
bonto11 <157644354+bonto11@users.noreply.github.com>
2025-11-07 09:20:32 +0100

    Update page.tsx

--- frontend/src/app/(protected)/recovery/hrv/page.tsx
+++ frontend/src/app/(protected)/recovery/hrv/page.tsx
@@ -1,9 +1,9 @@
+// src/app/(protected)/recovery/hrv/page.tsx
 "use client";
 
 import dynamic from "next/dynamic";
 import ButtonBack from "@/shared/components/ui/ButtonBack";
 
-// dynamický import komponentu s grafom
 const HRVDetailClient = dynamic(
   () => import("@/features/recovery/components/TrendHRV"),
   { ssr: false }
@@ -11,11 +11,16 @@ const HRVDetailClient = dynamic(
 
 export default function Page() {
   return (
-      <div className="max-w-screen-lg mx-auto px-3">
-        <ButtonBack title="Detail - Hearth Rate Variability - HRV" />
-        <div className="pt-3">
+    <div className="max-w-screen-lg mx-auto px-3 overflow-x-hidden">
+      <ButtonBack title="Detail - Hearth Rate Variability - HRV" />
+
+      {/* izoluj obsah, aby nič z vnútra nepretieklo doprava */}
+      <div className="pt-3 min-w-0">
+        {/* 1px guard proti iOS canvas overflowu */}
+        <div className="-mr-px">
           <HRVDetailClient />
         </div>
       </div>
+    </div>
   );
 }
