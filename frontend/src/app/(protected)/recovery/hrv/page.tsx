42028555028e12bf6c57777e72088ee3e967f378
z004es1x <PATRIKMBONTAR@GMAIL.COM>
2025-11-07 12:19:24 +0100

    page

--- frontend/src/app/(protected)/recovery/hrv/page.tsx
+++ frontend/src/app/(protected)/recovery/hrv/page.tsx
@@ -1,9 +1,9 @@
-// src/app/(protected)/recovery/hrv/page.tsx
 "use client";
 
 import dynamic from "next/dynamic";
 import ButtonBack from "@/shared/components/ui/ButtonBack";
 
+// dynamický import komponentu s grafom
 const HRVDetailClient = dynamic(
   () => import("@/features/recovery/components/TrendHRV"),
   { ssr: false }
@@ -11,11 +11,11 @@ const HRVDetailClient = dynamic(
 
 export default function Page() {
   return (
-    <div className="max-w-screen-lg mx-auto px-3 overflow-x-hidden">
-      <ButtonBack title="Detail - Hearth Rate Variability - HRV" />
-      <div className="pt-3">
-        <HRVDetailClient />
+      <div className="max-w-screen-lg mx-auto px-3">
+        <ButtonBack title="Detail — Resting Hearth Rate - RHR" />
+        <div className="pt-3">
+          <HRVDetailClient />
+        </div>
       </div>
-    </div>
   );
 }
