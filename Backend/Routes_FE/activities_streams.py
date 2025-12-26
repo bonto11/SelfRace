babfdf7c1e19c5f77960d9d3515ab8df9cd1f7cf
bonto11 <157644354+bonto11@users.noreply.github.com>
2025-12-26 23:32:03 +0100

    Update activities_streams.py

--- Backend/Routes_FE/activities_streams.py
+++ Backend/Routes_FE/activities_streams.py
@@ -1,25 +1,29 @@
-# Routes_FE/activities.py
+# Routes_FE/activities_streams.py
 
 from __future__ import annotations
 
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException, Depends
+
 from Services.activities_streams import (
     service_get_streams_one,
 )
+from Modules.Auth.deps import require_user_jwt
 
 router = APIRouter(prefix="/activities_streams", tags=["activities_streams"])
 
+
 @router.get("/{user_id}/{activity_id}")
 def get_streams_one(
-    activity_id: int,
     user_id: int,
+    activity_id: int,
+    user_jwt: str = Depends(require_user_jwt),
 ):
     try:
         streams = service_get_streams_one(
             user_id=user_id,
             activity_id=activity_id,
+            user_jwt=user_jwt,
         )
-        print(",streams",streams)
         return {"success": True, "streams": streams}
     except Exception as e:  # noqa: BLE001
         raise HTTPException(status_code=500, detail=str(e))
