2bfa71a50e20fe38ea68a9fe5eec0238b75a3567
bonto11 <157644354+bonto11@users.noreply.github.com>
2025-12-26 17:10:29 +0100

    Delete supabaseClient.ts

--- frontend/src/app/shared/utils/supabaseClient.ts
+++
@@ -1,9 +0,0 @@
-// src/shared/utils/supabaseClient.ts
-"use client";
-import { createBrowserClient } from "@supabase/ssr";
-import type { SupabaseClient } from "@supabase/supabase-js";
-
-export const supabaseClient: SupabaseClient = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
