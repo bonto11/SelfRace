921f7e53dd31fc7cfb401f7a258175572f55dcd6
bonto11 <157644354+bonto11@users.noreply.github.com>
2026-01-22 15:33:24 +0100

    Update page.tsx

--- frontend/src/app/(protected)/calendar/page.tsx
+++ frontend/src/app/(protected)/calendar/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import ActivitiesCalendar from "@/app/features/calendar/ActivitiesCalendar";
-import ButtonBack from "@/app/shared/components/ui/AppHeader";
+import AppHeader from "@/app/shared/components/ui/AppHeader";
 import { PAGE_CONTAINER, PAGE_STACK } from "@/app/shared/ui/tokens/pageTokens";
 
 export default function CalendarPage() {
   return (
     <div className={PAGE_CONTAINER}>
       <div className={PAGE_STACK}>
-        <ButtonBack title="Kalendár" showBack={false} container/>
+        <AppHeader title="Kalendár" showBack={false} container/>
         <ActivitiesCalendar />
       </div>
     </div>
