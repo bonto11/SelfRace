b268b93b08ac284b3f8ea6b0f2b2b2d354356107
z004es1x <PATRIKMBONTAR@GMAIL.COM>
2026-01-31 16:07:13 +0100

    edit

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,10 +13,10 @@ export const metadata: Metadata = {
     capable: true,
     statusBarStyle: "default",
     title: "SelfRace",
-    startupImage: "/logo/selfrace_logo_black_230.png",
+    startupImage: "/logo/selfrace_logo_black_260.png",
   },
   icons: {
-    apple: "/logo/selfrace_logo_black_230.png", // Toto je kľúčové pre iOS
+    apple: "/logo/selfrace_logo_black_260.png", // Toto je kľúčové pre iOS
   },
 };
 
